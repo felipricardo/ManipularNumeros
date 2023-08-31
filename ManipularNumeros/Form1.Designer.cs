3f41f3140a925de80804ab72ddb0452d5af1f6a5
felipricardo <felipebarbosaok@gmail.com>
2023-08-31 14:26:41 -0300

    Adicionar arquivos de projeto.

---
+++ ManipularNumeros/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ManipularNumeros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bntExecutar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bntExecutar
+            // 
+            this.bntExecutar.Location = new System.Drawing.Point(579, 416);
+            this.bntExecutar.Name = "bntExecutar";
+            this.bntExecutar.Size = new System.Drawing.Size(260, 74);
+            this.bntExecutar.TabIndex = 0;
+            this.bntExecutar.Text = "Executar";
+            this.bntExecutar.UseVisualStyleBackColor = true;
+            this.bntExecutar.Click += new System.EventHandler(this.bntExecutar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(12, 9);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(827, 404);
+            this.lblResultado.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 511);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.bntExecutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bntExecutar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
